dec3739d42b97c4c8ea930602800b4be679b0a3a
Drew Hjelm <drew.hjelm@gmail.com>
2018-02-08 10:48:19 -0600

    initial commit

---
+++ Parse-FortiGateRules.ps1
@@ -0,0 +1,108 @@
+<#
+.SYNOPSIS
+Parse-FortiGateRules parses rules from a FortiGate device into a CSV file.
+.DESCRIPTION
+The Parse-FortiGateRules reads a FortiGate config file and pulls out the rules for each VDOM in the file into a CSV.
+.PARAMETER NessusFilePath
+[REQUIRED] This is the path to the FortiGate config file
+.EXAMPLE
+.\Parse-FortiGateRules.ps1 -fortiGateConfig "c:\temp\config.conf"
+Parses a FortiGate config file and places the CSV file in the same folder where the config was found.
+.NOTES
+Author: Drew Hjelm (@drewhjelm)
+Last Modified: 02/08/2018
+#>
+Param
+(
+    [Parameter(Mandatory = $true)]
+    [string]$fortigateConfig
+)
+#need some empty items to load config
+$loadedConfig;
+$workingFolder;
+$fileName;
+
+$fileCount = 0;
+
+if ([System.IO.File]::Exists($fortigateConfig) -eq $false) {
+    Write-Output "[!] ERROR: Could not find FortiGate config file at $fortigateConfig."
+    exit
+}
+else {
+    $loadedConfig = Get-Content $fortigateConfig;
+    $workingFolder = Split-Path $fortigateConfig;
+    $fileName = Split-Path $fortigateConfig -Leaf;
+}
+
+#all known policy properties in fortigate rules. need this to ensure that data isn't missing in spreadsheet.
+$dummyRule = Get-Content .\fortiDummyRule.txt
+
+
+#initialize an empty item and object array to be used in the loop below
+$ruleList = New-Object System.Collections.ArrayList;
+$rule;
+$policySection = $false;
+$vdomConfig = $false;
+$vdom;
+
+$modifiedConfig = @();
+
+foreach ($line in $loadedConfig) {
+    #look for the firewall policy section of the config
+    if ($line.Trim() -eq "config firewall policy") {
+        #need to pull in a dummy rule with all the known policy properties
+        $modifiedConfig += $line;
+        foreach ($prop in $dummyRule)
+        {
+            $modifiedConfig += $prop;
+
+        }
+        continue;
+    }
+    else {
+        $modifiedConfig += $line;
+    }
+}
+
+foreach ($line in $modifiedConfig) {
+    #look for the firewall policy section of the config
+    if ($line.Trim() -eq "config firewall policy") {
+        $policySection = $true;
+        continue;
+    }
+    #look for vdom config
+    if ($line.Trim() -match "^config vdom") {
+        $vdomConfig = $true;
+        continue;
+    } 
+    #look for vdom config
+    if (($line.Trim() -match "^edit") -and ($vdomConfig)) {
+        $lineSplit = $line.Trim().Split();
+        $vdom = $lineSplit[1];
+        $vdomConfig = $false;
+        continue;
+    }
+    #look for the beginning of a firewall rule and create a new rule with an ID of the rule ID
+    if (($line.Trim() -match "^edit ") -and ($policySection)) {
+        $rule = New-Object System.Object;
+        $lineSplit = $line.Trim().Split();
+        $rule | Add-Member -MemberType NoteProperty -Name "id" -Value $lineSplit[1];
+    }
+    #pull the rule property and add that to the rule
+    elseif (($line.Trim() -match "^set ") -and ($policySection)) {
+        $lineSplit = $line.Trim().Split();
+        $value = "$($lineSplit | Select-Object -Skip 2)"
+        $rule | Add-Member -MemberType NoteProperty -Name $lineSplit[1] -Value $value;
+    }
+    #when the rule end is found, add the rule to the list
+    elseif (($line.Trim() -match "^next") -and ($policySection)) {
+        $ruleList.Add($rule);
+    }
+    if (($line.Trim() -match "^end") -and ($policySection)) {
+        $policySection = $false;
+        $date = Get-Date -Format yyyyMMddhhmmss
+        $ruleList | Export-Csv "$workingFolder\rules-$fileName-$vdom-$date-$fileCount.csv"
+        $fileCount++;
+        $vdom = $null;
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# firewall-audit
+This is a collection of scripts to assist in auditing firewall rules.
+
+Parse-FortiGateRules reads a FortiGate config file and pulls out the rules for each VDOM in the file into a CSV. 
+
+Note: the fortiDummyRule.txt is required to populate all the fields into a PS object for each of the rules being added to the CSV. 
+
+EXAMPLE 
+.\Parse-FortiGateRules.ps1 -fortiGateConfig "c:\temp\config.conf" 
+Parses a FortiGate config file and places the CSV file in the same folder where the config was found. 
